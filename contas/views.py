0e206e69fe2f22d9e43d17584ad02f293d18a9b9
Anselmo Junior <anselmojuniorjj@gmail.com>
2019-04-02 10:47:55 -0300

    Basic Django

---
+++ contas/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
+
+# Create your views here.
+
+def home(request):
+    data = {}
+    data['transacoes'] = ['t1', 't2', 't3']
+    data['now'] = datetime.datetime.now()
+    #html = "<html><body>It is now %s.</body></html>" %now
+    return render(request, 'contas/home.html', data)
